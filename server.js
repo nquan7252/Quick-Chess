92bf78c5eeefb8df644f80c70210f5038a194047
Quan <nquan7252@gmail.com>
2021-11-22 20:11:04 -0800

    e Please enter whw commif message for your changes. Lines starting

---
+++ server.js
@@ -0,0 +1,29 @@
+const path=require('path');
+const http=require('http');
+const express=require("express");
+const socketio=require('socket.io');
+var isWhite=true;
+var whiteConnected=false;
+var blackConnected=false;
+const app=express();
+const server=http.createServer(app);
+const io=socketio(server);
+app.use(express.static(path.join(__dirname,'public')));
+io.on('connection',socket =>{
+    if (isWhite){
+        console.log("White player connected");
+        isWhite=!isWhite;
+        whiteConnected=true;
+    }
+    else{ 
+        console.log("Black player connected");
+        blackConnected=true;
+        isWhite=!isWhite;
+    }
+
+   socket.on("move",(chosenPieceName,chosenPieceColor,oldposition,newposition,src)=>{
+       io.emit("updatemove",chosenPieceName,chosenPieceColor,oldposition,newposition,src);
+   })
+
+})
+server.listen(8888,()=>console.log("running on 8888")); 
